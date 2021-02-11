92619f0313a92c07be3e6c9e6af2dbe900479d06
Willy Malaga <will29072607@hotmail.com>
2021-02-11 16:57:06 -0500

    Fin sección 15

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -5,9 +5,9 @@ import { Observable, of } from 'rxjs';
 import { Router } from '@angular/router';
 import { environment } from '../../environments/environment';
 
-
 import { RegisterForm } from '../interfaces/register-form.interface';
 import { LoginForm } from '../interfaces/login-form.interface';
+import { Usuario } from '../models/usuario.model';
 
 const base_url = environment.base_url;
 
@@ -17,73 +17,98 @@ declare const gapi: any;
   providedIn: 'root',
 })
 export class UsuarioService {
-
   public auth2: any;
+  public usuario: Usuario;
 
-  constructor(private http: HttpClient, private router: Router, private ngZone: NgZone) {
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private ngZone: NgZone
+  ) {
     this.googleInit();
   }
 
-  googleInit(){
+  get token(): string {
+    return localStorage.getItem('token') || '';
+  }
 
-    return new Promise( resolve => {
+  get uid(): string {
+    return this.usuario.uid || '';
+  }
 
-        gapi.load('auth2', () => {
+  googleInit() {
+    return new Promise((resolve) => {
+      gapi.load('auth2', () => {
         this.auth2 = gapi.auth2.init({
-          client_id: '338683424287-36bblp3uqmdlpg2tnhc2noshv5erclr5.apps.googleusercontent.com',
+          client_id:
+            '338683424287-36bblp3uqmdlpg2tnhc2noshv5erclr5.apps.googleusercontent.com',
           cookiepolicy: 'single_host_origin',
         });
         resolve();
       });
-    })     
+    });
   }
 
   logout() {
-    localStorage.removeItem('token');    
-    this.auth2.signOut().then( () => {
+    localStorage.removeItem('token');
+    this.auth2.signOut().then(() => {
       this.ngZone.run(() => {
         this.router.navigateByUrl('/login');
-      })      
+      });
     });
   }
 
   validarToken(): Observable<boolean> {
-    const token = localStorage.getItem('token') || '';
-    return this.http.get(`${base_url}/login/renew`, {
-      headers: {
-        'x-token': token
-      }
-    }).pipe(
+    return this.http
+      .get(`${base_url}/login/renew`, {
+        headers: {
+          'x-token': this.token,
+        },
+      })
+      .pipe(
+        map((resp: any) => {
+          const { email, google, nombre, role, img = '', uid } = resp.usuario;
+
+          this.usuario = new Usuario(nombre, email, '', img, google, role, uid);
+          localStorage.setItem('token', resp.token);
+          return true;
+        }),
+        catchError((error) => of(false))
+      );
+  }
+
+  crearUsuario(formData: RegisterForm) {
+    return this.http.post(`${base_url}/usuarios`, formData).pipe(
       tap((resp: any) => {
         localStorage.setItem('token', resp.token);
-      }),
-      map(resp => true),
-      catchError(error => of(false))
+      })
     );
   }
 
-  crearUsuario(formData: RegisterForm){
-    return this.http.post(`${base_url}/usuarios`,formData)
-    .pipe(
-      tap( (resp: any) => {
-        localStorage.setItem('token', resp.token)
-      })
-    )
+  actualizarPerfil(data: { email: string, nombre: string, role: string }) {
+    data = {
+      ...data,
+      role: this.usuario.role
+    };
+
+    return this.http.put(`${base_url}/usuarios/${this.uid}`, data, {
+      headers: {
+        'x-token': this.token,
+      },
+    });
   }
 
   login(formData: LoginForm) {
-    return this.http.post(`${base_url}/login`, formData)
-    .pipe(
-      tap( (resp: any) => {
-        localStorage.setItem('token', resp.token)
+    return this.http.post(`${base_url}/login`, formData).pipe(
+      tap((resp: any) => {
+        localStorage.setItem('token', resp.token);
       })
     );
   }
   loginGoogle(token) {
-    return this.http.post(`${base_url}/login/google`, {token})
-    .pipe(
-      tap( (resp: any) => {
-        localStorage.setItem('token', resp.token)
+    return this.http.post(`${base_url}/login/google`, { token }).pipe(
+      tap((resp: any) => {
+        localStorage.setItem('token', resp.token);
       })
     );
   }
