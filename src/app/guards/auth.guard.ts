77428ec5cfcebc0c9636df8ae439b4bc336ed760
Willy Malaga <will29072607@hotmail.com>
2021-04-20 18:45:34 -0500

    Environment listo

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,28 +1,43 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router,
+  CanLoad,
+  Route,
+  UrlSegment,
+} from '@angular/router';
 import { UsuarioService } from '../services/usuario.service';
 import { tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
+  constructor(private usuarioService: UsuarioService, private router: Router) {}
 
-  constructor(private usuarioService: UsuarioService, private router: Router){
-
-  }
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot) {
-
-    return this.usuarioService.validarToken()
-    .pipe(
-      tap(estaAutenticado => {
-        if(!estaAutenticado){
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]
+  ): boolean | Observable<boolean> | Promise<boolean> {
+    return this.usuarioService.validarToken().pipe(
+      tap((estaAutenticado) => {
+        if (!estaAutenticado) {
           this.router.navigateByUrl('/login');
         }
       })
     );
   }
-  
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    return this.usuarioService.validarToken().pipe(
+      tap((estaAutenticado) => {
+        if (!estaAutenticado) {
+          this.router.navigateByUrl('/login');
+        }
+      })
+    );
+  }
 }
